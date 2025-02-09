177de0cb46c717d90aa9211764dbeface07b02c0
sanjeetk-dev <sanjeetk.dev@gmail.com>
2025-02-09 20:47:55 +0530

    Adding server/src/models/review.model.js

---
+++ server/src/models/review.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    title: {
+      type: String,
+      trim: true,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+    helpfulVotes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model("Review", reviewSchema);
+
