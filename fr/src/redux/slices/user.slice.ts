efc3ca6713d7f16c06465bf1c4b88754fb7c8723
sanjeetk-dev <sanjeetk.dev@gmail.com>
2025-02-09 20:47:10 +0530

    Adding fr/src/redux/slices/user.slice.ts

---
+++ fr/src/redux/slices/user.slice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null, // Stores user data
+  isAuthenticated: false, // Tracks login status
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    clearUser: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
