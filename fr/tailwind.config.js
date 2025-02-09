ac43848c152864e00d22d207b7bede558a3caeef
sanjeetk-dev <sanjeetk.dev@gmail.com>
2025-02-09 20:47:13 +0530

    Adding fr/tailwind.config.js

---
+++ fr/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#d97aff", // Soft Purple
+        background: "#f5f0ff", // Lavender Mist
+        accent: "#ffb6ff", // Light Pink
+        text: "#333333", // Dark Gray
+        cta: "#ff8e44", // Peach Orange
+        navbar: "#faf5ff", // Soft White
+        sidebar: "#faf5ff", // Light Lavender
+      },
+    },
+  },
+  plugins: [],
+};
